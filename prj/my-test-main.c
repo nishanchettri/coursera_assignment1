c6059162a41aca6dea00a65b4c2cf627744c7d46
nishan <nishanchettri@gmail.com>
2020-05-31 11:28:38 +0530

    modified:   cards.c 	modified:   my-test-main.c

--- prj/my-test-main.c
+++ prj/my-test-main.c
@@ -1,5 +1,12 @@
 #include "cards.h"
 
 int main(void) {
+	
+assert_card_valid(card_t c);
+ranking_to_string(hand_ranking_t r) ;
+value_letter(card_t c);
+suit_letter(card_t c) ;
+print_card(card_t c);
+card_from_letters(char value_let, char suit_let);
 
 }
